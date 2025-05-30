1725896d48a3f89e96e3d2403496765b9bdb92a8
Carlos Oliveira <kaduchaos18@gmail.com>
2025-05-30 19:32:39 -0300

    add hero and import Avenir font

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -1,9 +1,15 @@
-import gulp, { watch } from 'gulp';
+import gulp from 'gulp';
+import imagemin from 'gulp-imagemin';
 import * as dartSass from 'sass';
 import gulpSass from 'gulp-sass';
 
 const sass = gulpSass(dartSass);
 
+function images() {
+    return gulp.src('./src/images/**/*', { encoding:false,})
+        .pipe(imagemin())
+        .pipe(gulp.dest('./dist/images'))
+};
 
 function styles() {
     return gulp.src('./src/styles/*.scss')
@@ -13,9 +19,10 @@ function styles() {
 
 function watchFiles() {
     gulp.watch('./src/styles/**/*.scss', styles);
+    gulp.watch('./src/images', images);
 }
 
-export {styles};
+export {styles, images};
 
 gulp.task('default', gulp.series(
-    gulp.parallel(styles), watchFiles));
+    gulp.parallel(styles, images), watchFiles));
